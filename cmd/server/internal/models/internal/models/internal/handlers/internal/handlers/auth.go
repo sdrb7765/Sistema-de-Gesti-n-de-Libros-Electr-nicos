d311ff21ae224405a5c28ae949b864d9642cc4ef
sdrb7765 <160300752+sdrb7765@users.noreply.github.com>
2025-12-23 22:46:06 -0500

    Create auth.go

---
+++ cmd/server/internal/models/internal/models/internal/handlers/internal/handlers/auth.go
@@ -0,0 +1,16 @@
+package handlers
+
+import (
+	"html/template"
+	"net/http"
+)
+
+func Login(w http.ResponseWriter, r *http.Request) {
+	tmpl := template.Must(template.ParseFiles("templates/login.html"))
+	tmpl.Execute(w, nil)
+}
+
+func Register(w http.ResponseWriter, r *http.Request) {
+	tmpl := template.Must(template.ParseFiles("templates/register.html"))
+	tmpl.Execute(w, nil)
+}
